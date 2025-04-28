98c575fa89952da25ea7f093da0aa62a178b29be
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-28 13:25:48 +0530

    [Siddharth] ADD: Initialized the project

---
+++ src/test/java/com/cg/estate_racker/EstateRackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.estate_racker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateRackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
